4c7be2c8131a9dc209c57ce6a41bb944dcb57d64
ahmed inspon <ahmed.quereshi@inspon.com>
2023-05-04 11:12:54 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const website_url = 'https://www.bannerbear.com/blog/how-to-download-images-from-a-website-using-puppeteer/';
+
+const init = async() => {
+    // Create a browser instance
+    const browser = await puppeteer.launch();
+    // Create a new page
+    const page = await browser.newPage();
+    await page.goto(website_url, { waitUntil: 'networkidle0' });
+    const html = fs.readFileSync('packaging_slip.html', 'utf-8');
+    await page.setContent(html, { waitUntil: 'domcontentloaded' });
+    await page.emulateMediaType('screen');
+
+    // Downlaod the PDF
+    const pdf = await page.pdf({
+        path: 'result.pdf',
+        margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+        printBackground: true,
+        format: 'A4',
+    });
+
+    // Close the browser instance
+    await browser.close();
+}
+// Open URL in current page
+init();
